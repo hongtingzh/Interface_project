ff923aaf5f7732dae0773ceae61acbf2d7e0dba3
hongting <1462367817@qq.com>
2022-01-27 22:50:38 +0800

    test pycharm commit~

--- chapter7/generator.py
+++ chapter7/generator.py
@@ -3,3 +3,12 @@
 # 信息类（构造个人信息数据和表单信息数据：姓名、地址、电话、工作、证件号、银行卡、公司）
 # 网络类（构造IP MAC HTTP的客户端类型和文件类型、反反爬）
 
+
+from faker import Factory
+
+
+fake = Factory.create('zh_CN')
+
+
+def random_python_data():
+    return fake.pystr()
